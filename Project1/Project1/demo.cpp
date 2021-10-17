750c1879e56ab7c7bd4d56f40d103a8bf14e16af
JustDuck1 <viet1432@gmail.com>
2021-10-18 02:44:26 +0700

    add Project1

---
+++ Project1/Project1/demo.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int a,b;
+	float tong, hieu, tich, thuong;
+	cout << "nhap a: ";
+	cin >> a;
+	cout << "nhap b: ";
+	cin >> b;
+	tong = a + b;
+	hieu = a - b;
+	tich = a * b;
+	thuong = a / b;
+	cout << "tong = " << tong << endl;
+	cout << "hieu = " << hieu << endl;
+	cout << "tich = " << tich << endl;
+	cout << "thuong = " << thuong << endl;
+}
+
+
